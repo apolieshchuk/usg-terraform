f2b82cc916e9da79233f1e2ab81afa5f9a26d55d
a.poleshchuk <a.poleshchuk@selectoglboal.com>
2022-10-06 00:36:46 +0300

    add some network fixes

--- networking.tf
+++ networking.tf
@@ -1,3 +1,16 @@
+/* Resources that will be created will be defined inside of the VPC.
+ An AWS VPC provides logical isolation of resources from one another.
+ All of the resources that will be defined will live within the same VPC */
+resource "aws_vpc" "aws-vpc" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name        = "${var.app_name}-vpc"
+    Environment = terraform.workspace
+  }
+}
+
 // allows communication between the VPC and the internet at all
 resource "aws_internet_gateway" "aws-igw" {
   vpc_id = aws_vpc.aws-vpc.id
@@ -61,33 +74,36 @@ resource "aws_route_table_association" "public" {
  the NAT gateway is added to the private subnets defined earlier. */
 
 // Elastic IP (get exist from aws directly)
-data "aws_eip" "eip" {
-  tags = {
-    Name = "${var.app_name}-${terraform.workspace}-eip"
-  }
-}
-
-// Or create new
-//resource "aws_eip" "gateway" {
-//  count      = 1
-//  vpc        = true
-//  depends_on = [aws_internet_gateway.aws-igw]
-//
+//data "aws_eip" "eip" {
 //  tags = {
 //    Name = "${var.app_name}-${terraform.workspace}-eip"
 //  }
 //}
+
+// Or create new
+resource "aws_eip" "gateway" {
+  count      = 1
+  vpc        = true
+  depends_on = [aws_internet_gateway.aws-igw]
+
+  tags = {
+    Name = "${var.app_name}-${terraform.workspace}-eip"
+  }
+}
 
 /* The NAT gateway allows resources within the VPC to communicate with
 the internet but will prevent communication to the VPC from outside sources */
 resource "aws_nat_gateway" "gateway" {
   count         = 1
   subnet_id     = element(aws_subnet.public.*.id, count.index)
-//  allocation_id = element(aws_eip.gateway.*.id, count.index)// ToDo
-  allocation_id = data.aws_eip.eip.id
+//  allocation_id = data.aws_eip.eip.id
+  allocation_id = element(aws_eip.gateway.*.id, count.index) // ToDo
   tags = {
     Name = "${var.app_name}-${terraform.workspace}-nat"
   }
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.aws-igw]
 }
 
 resource "aws_route_table" "private" {
@@ -108,4 +124,8 @@ resource "aws_route_table_association" "private" {
   count          = 1
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(aws_route_table.private.*.id, count.index)
+}
+
+output "gateway_elastic_ip" {
+  value = aws_eip.gateway.*.public_ip
 }
